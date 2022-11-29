b7a27aa5f86fea0e67eb6708c7e8b413450207b6
Maksim Skalaban <m.skalaban@gmail.com>
2022-11-29 13:58:56 +0300

    remove default components, add VComponents

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <VLink :link="'/'">Home</VLink>
+    <VLink :link="'/about'">About</VLink>
   </nav>
   <router-view/>
 </template>
@@ -15,16 +15,10 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+<script>
+import VLink from "@/components/VComponents/VLink";
+export default {
+  components: {VLink}
+}
+</script>
---
+++ src/components/VComponents/VLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link :to="$props.link">
+    <slot/>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "VLink",
+  pops: {
+    link: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
